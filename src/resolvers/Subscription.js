572c51078d3dfbc343eb235038309702f2c1886f
KazChe <kaz.che@gmail.com>
2020-09-18 16:31:40 -0700

    just to keep both laptops in sync

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -24,7 +24,6 @@ const Subscription = {
     },
     post: {
         subscribe(parent, args, { db, pubsub }, info) {
-            console.log('got here')
             return pubsub.asyncIterator('post')
             // next we have to publish newly created post
             // this will be done when the post create mutation is run, see Mutations.createPost()
