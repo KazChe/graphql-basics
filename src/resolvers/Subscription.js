a6ac62a0ec7fd0b191c1e0a5bccc2c80d12f4592
KazChe <kaz.che@gmail.com>
2020-09-18 18:16:36 -0700

    Exploring GraphQLServer info object

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -25,6 +25,7 @@ const Subscription = {
     post: {
         subscribe(parent, args, { db, pubsub, kimia }, info) {
             console.log(kimia)
+            console.dir(info)
             return pubsub.asyncIterator('post')
             // next we have to publish newly created post
             // this will be done when the post create mutation is run, see Mutations.createPost()
